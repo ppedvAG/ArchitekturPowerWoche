ffd03b6e246df869822478e6c35677260bcea3a8
André <AndreR@ppedv.de>
2022-06-28 14:18:24 +0200

    AutoFixture und FluentAssertions sind ein gutes Team

--- HalloEfCore/HalloEfCore.Tests/Data/EfContextTests.cs
+++ HalloEfCore/HalloEfCore.Tests/Data/EfContextTests.cs
@@ -1,7 +1,10 @@
-﻿using HalloEfCore.Data;
+﻿using AutoFixture;
+using AutoFixture.Kernel;
+using FluentAssertions;
+using HalloEfCore.Data;
 using HalloEfCore.Model;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
 
 namespace HalloEfCore.Tests.Data
 {
@@ -18,7 +21,8 @@ namespace HalloEfCore.Tests.Data
 
             var result = con.Database.EnsureCreated();
 
-            Assert.IsTrue(result);
+            //Assert.IsTrue(result);
+            result.Should().BeTrue();
         }
 
         [TestMethod]
@@ -32,18 +36,21 @@ namespace HalloEfCore.Tests.Data
             {
                 con.Mitarbeiter.Add(m);
                 var result = con.SaveChanges();
-                Assert.AreEqual(2, result);
+                //Assert.AreEqual(2, result);
+                result.Should().Be(2);
             }
 
             //READ + UPDATE
             using (var con = new EfContext(ConString))
             {
                 var loaded = con.Mitarbeiter.Find(m.Id);
-                Assert.AreEqual(m.Name, loaded.Name);
+                //Assert.AreEqual(m.Name, loaded.Name);
+                loaded.Name.Should().Be(m.Name);
 
                 loaded.Name = newName;
                 var result = con.SaveChanges();
-                Assert.AreEqual(1, result);
+                //Assert.AreEqual(1, result);
+                result.Should().Be(1);
             }
 
             //check UPDATE + DELETE
@@ -51,19 +58,64 @@ namespace HalloEfCore.Tests.Data
             {
                 var loaded = con.Mitarbeiter.Find(m.Id);
                 Assert.AreEqual(newName, loaded.Name);
+                loaded.Name.Should().Be(newName);
 
                 con.Remove(loaded);
                 var result = con.SaveChanges();
-                Assert.AreEqual(2, result);
+                //Assert.AreEqual(2, result);
+                result.Should().Be(2);
             }
 
             //check DELETE
             using (var con = new EfContext(ConString))
             {
                 var loaded = con.Mitarbeiter.Find(m.Id);
-                Assert.IsNull(loaded);
+                //Assert.IsNull(loaded);
+                loaded.Should().BeNull();
             }
 
+        }
+
+        [TestMethod]
+        public void Can_create_read_Mitarbeiter_AutoFix()
+        {
+            var fix = new Fixture();
+            fix.Behaviors.Add(new OmitOnRecursionBehavior());
+            fix.Customizations.Add(new PropertyNameOmitter("Id"));
+            var m = fix.Create<Mitarbeiter>();
+
+            using (var con = new EfContext(ConString))
+            {
+                con.Add(m);
+                con.SaveChanges();
+            }
+
+            using (var con = new EfContext(ConString))
+            {
+                var loaded = con.Mitarbeiter.Include(x => x.Kunden).Include(x => x.Abteilungen).First(x => x.Id == m.Id);
+                loaded.Should().BeEquivalentTo(m, x => x.IgnoringCyclicReferences());
+            }
+
+        }
+
+
+        internal class PropertyNameOmitter : ISpecimenBuilder
+        {
+            private readonly IEnumerable<string> names;
+
+            internal PropertyNameOmitter(params string[] names)
+            {
+                this.names = names;
+            }
+
+            public object Create(object request, ISpecimenContext context)
+            {
+                var propInfo = request as PropertyInfo;
+                if (propInfo != null && names.Contains(propInfo.Name))
+                    return new OmitSpecimen();
+
+                return new NoSpecimen();
+            }
         }
 
     }
