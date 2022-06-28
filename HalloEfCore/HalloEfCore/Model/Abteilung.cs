d338df3d8e0c281cff63770d810f157372ecebcf
André <AndreR@ppedv.de>
2022-06-28 10:21:14 +0200

    Model für EfCore

---
+++ HalloEfCore/HalloEfCore/Model/Abteilung.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace HalloEfCore.Model
+{
+    public class Abteilung
+    {
+        public int Id { get; set; }
+        public string Bezeichnung { get; set; }
+        public ICollection<Mitarbeiter> Mitarbeiter { get; set; } = new HashSet<Mitarbeiter>();
+    }
+}
---
+++ HalloEfCore/HalloEfCore/Model/Kunde.cs
@@ -0,0 +1,8 @@
+﻿namespace HalloEfCore.Model
+{
+    public class Kunde : Person
+    {
+        public string KdNummer { get; set; }
+        public Mitarbeiter Ansprechpartner { get; set; }
+    }
+}
