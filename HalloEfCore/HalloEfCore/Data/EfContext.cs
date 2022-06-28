225f557f17b8a0332ef6857ca6f4b6b07fd00e02
André <AndreR@ppedv.de>
2022-06-28 11:53:33 +0200

    CRUD sind fertig

--- HalloEfCore/HalloEfCore/Data/EfContext.cs
+++ HalloEfCore/HalloEfCore/Data/EfContext.cs
@@ -8,10 +8,20 @@ namespace HalloEfCore.Data
         public DbSet<Abteilung> Abteilungen { get; set; }
         public DbSet<Kunde> Kunden { get; set; }
         public DbSet<Mitarbeiter> Mitarbeiter { get; set; }
+        public DbSet<Person> Persons { get; set; }
 
         public EfContext()
         {
-            Database.EnsureCreated();
+            //Database.EnsureDeleted();
+            //Database.EnsureCreated();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Person>().ToTable("Person");
+            modelBuilder.Entity<Mitarbeiter>().ToTable("Mitarbeiter");
+            modelBuilder.Entity<Kunde>().ToTable("Kunden");
+            modelBuilder.Entity<Mitarbeiter>().Property(x => x.Beruf).HasColumnName("BBBBBBBBeruf");
         }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
--- HalloEfCore/HalloEfCore/MainWindow.xaml.cs
+++ HalloEfCore/HalloEfCore/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using HalloEfCore.Data;
+using HalloEfCore.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.EntityFrameworkCore;
 
 namespace HalloEfCore
 {
@@ -23,6 +26,74 @@ namespace HalloEfCore
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        EfContext context = new EfContext();
+
+        private void LoadMitarbeiter(object sender, RoutedEventArgs e)
+        {
+            myGrid.ItemsSource = context.Mitarbeiter.Where(x => x.Name.StartsWith("F"))
+                                                    .Include(x => x.Abteilungen)
+                                                    .ToList();
+        }
+
+        private void CreateDemoData(object sender, RoutedEventArgs e)
+        {
+            var abt1 = new Abteilung() { Bezeichnung = "Holz" };
+            var abt2 = new Abteilung() { Bezeichnung = "Steine" };
+
+            for (int i = 0; i < 100; i++)
+            {
+                var m = new Mitarbeiter()
+                {
+                    Name = $"Fred #{i:000}",
+                    GebDatum = DateTime.Now.AddDays(i * 17).AddYears(-40),
+                    Beruf = "Macht dinge",
+                };
+
+                if (i % 2 == 0)
+                    m.Abteilungen.Add(abt1);
+
+                if (i % 3 == 0)
+                    m.Abteilungen.Add(abt2);
+
+                context.Add(m);
+            }
+            context.SaveChanges();
+        }
+
+        private void NewMitarbeiter(object sender, RoutedEventArgs e)
+        {
+            var m = new Mitarbeiter()
+            {
+                Name = $"Frida",
+                GebDatum = DateTime.Now.AddDays( 17).AddYears(-40),
+                Beruf = "Ist die Cheffin",
+            };
+
+            context.Add(m);
+            context.SaveChanges();
+        }
+
+        private void Save(object sender, RoutedEventArgs e)
+        {
+            context.SaveChanges();
+        }
+
+        private void DeleteSelectedMitarbeiter(object sender, RoutedEventArgs e)
+        {
+            if(myGrid.SelectedItem  is Mitarbeiter m)
+            {
+                var dlg = MessageBox.Show($"Soll {m.Name} wirklich gelöscht werden?", 
+                                          "",
+                                          MessageBoxButton.YesNo);
+
+                if(dlg == MessageBoxResult.Yes)
+                {
+                    context.Remove(m);
+                    context.SaveChanges();
+                }
+            }
         }
     }
 }
