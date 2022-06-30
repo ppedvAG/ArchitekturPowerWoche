feeafa9506be97ee56ea5ccff59825313403eecc
André <AndreR@ppedv.de>
2022-06-30 09:48:44 +0200

    Deko Pattern

---
+++ HalloDekorator/HalloDekorator/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using HalloDekorator;
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+Console.WriteLine("Hello, World!");
+
+var meinBrot = new Käse(new Brot());
+Console.WriteLine($"{meinBrot.Beschreibung} {meinBrot.Preis:c}");
+
+var meinPizza = new Salami( new Käse(new Käse(new Pizza())));
+Console.WriteLine($"{meinPizza.Beschreibung} {meinPizza.Preis:c}");
