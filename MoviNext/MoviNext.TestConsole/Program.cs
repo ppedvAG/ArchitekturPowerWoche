f073b6c0ff183ed4d4c1f7d2a1be343555a4b537
André <AndreR@ppedv.de>
2022-06-30 14:42:47 +0200

    Di mit AutoFac

--- MoviNext/MoviNext.TestConsole/Program.cs
+++ MoviNext/MoviNext.TestConsole/Program.cs
@@ -1,14 +1,31 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Autofac;
+using MoviNext.Data.EfCore;
 using MoviNext.HardwareService;
 using MoviNext.Model;
+using MoviNext.Model.Contracts;
+using System.Reflection;
 
 Console.WriteLine("Hello, World!");
 
-//DI manuell via Project Reference
-var hs = new HardwareService(new MoviNext.Data.EfCore.EfRepository());
+//DI manuel via Project Reference
+//var hs = new HardwareService(new MoviNext.Data.EfCore.EfRepository());
+
+//DI manuel via Reflection
+//var pfad = @"C:\Users\Fred\source\repos\ppedvAG\ArchitekturPowerWoche\MoviNext\MoviNext.Data.EfCore\bin\Debug\net6.0\MoviNext.Data.EfCore.dll";
+//var ass = Assembly.LoadFrom(pfad);
+//var efRepoType = ass.GetTypes().FirstOrDefault(x => x.GetInterfaces().Contains(typeof(IRepository)));
+//var repo = (IRepository)Activator.CreateInstance(efRepoType);
+//var hs = new HardwareService(repo);
+
+//DI per Autofac
+var builder = new ContainerBuilder();
+builder.RegisterType<EfRepository>().As<IRepository>();
+var container =  builder.Build();
+
+var hs = new HardwareService(container.Resolve<IRepository>());
 
 Console.WriteLine($"Most Leistung: {hs.GetUmrichterWithMostLeistung().Name}");
-
 
 foreach (var umr in hs.Repository.Query<Umrichter>().Where(x => x.Leistung > 2).ToList())
 {
