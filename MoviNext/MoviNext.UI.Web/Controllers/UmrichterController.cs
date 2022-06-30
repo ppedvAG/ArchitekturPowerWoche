5527dd49a935ce68f4401f13ecb09aaf617c01f5
André <ruland@outlook.de>
2022-06-30 16:59:27 +0200

    ASP MVC Seiten

--- MoviNext/MoviNext.UI.Web/Controllers/UmrichterController.cs
+++ MoviNext/MoviNext.UI.Web/Controllers/UmrichterController.cs
@@ -9,6 +9,11 @@ namespace MoviNext.UI.Web.Controllers
     {
         IRepository repo;
 
+        public UmrichterController(IRepository repo)
+        {
+            this.repo = repo;
+        }
+
         // GET: UmrichterController
         public ActionResult Index()
         {
@@ -18,22 +23,26 @@ namespace MoviNext.UI.Web.Controllers
         // GET: UmrichterController/Details/5
         public ActionResult Details(int id)
         {
-            return View();
+            return View(repo.GetById<Umrichter>(id));
         }
 
         // GET: UmrichterController/Create
         public ActionResult Create()
         {
-            return View();
+            return View(new Umrichter() { Frequenz = 100, Leistung = 4, LeistungsEinheit = LeistungsEinheit.MW });
         }
 
         // POST: UmrichterController/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
+        public ActionResult Create(Umrichter um)
         {
             try
             {
+                um.Steuerung = new Steuerung();
+                repo.Add(um);
+                repo.SaveChanges();
+
                 return RedirectToAction(nameof(Index));
             }
             catch
@@ -45,16 +54,20 @@ namespace MoviNext.UI.Web.Controllers
         // GET: UmrichterController/Edit/5
         public ActionResult Edit(int id)
         {
-            return View();
+            return View(repo.GetById<Umrichter>(id));
+
         }
 
         // POST: UmrichterController/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
+        public ActionResult Edit(int id, Umrichter umrichter)
         {
             try
             {
+                repo.Update(umrichter);
+                repo.SaveChanges();
+
                 return RedirectToAction(nameof(Index));
             }
             catch
@@ -66,16 +79,22 @@ namespace MoviNext.UI.Web.Controllers
         // GET: UmrichterController/Delete/5
         public ActionResult Delete(int id)
         {
-            return View();
+            return View(repo.GetById<Umrichter>(id));
+
         }
 
         // POST: UmrichterController/Delete/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
+        public ActionResult Delete(int id, Umrichter umrichter)
         {
             try
             {
+                //var toKill = repo.GetById<Umrichter>(id);
+                repo.Delete(umrichter);
+                repo.SaveChanges();
+
+
                 return RedirectToAction(nameof(Index));
             }
             catch
--- MoviNext/MoviNext.UI.Web/Program.cs
+++ MoviNext/MoviNext.UI.Web/Program.cs
@@ -1,7 +1,10 @@
+using MoviNext.Model.Contracts;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddScoped<IRepository, MoviNext.Data.EfCore.EfRepository>();
 
 var app = builder.Build();
 
